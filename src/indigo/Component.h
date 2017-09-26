7064522b58db8a86564759b1d374e1dc8ddd74f7
FairZ <fairsy2104@gmail.com>
2017-09-26 14:57:56 +0100

    Added more to component and created Transform

--- src/indigo/Component.h
+++ src/indigo/Component.h
@@ -1,13 +1,28 @@
 #ifndef __IND_COMPONENT__
 #define __IND_COMPONENT__
 
+class GameObj;
+
 class Component
 {
 
 protected:
+
+  GameObj* parentObj;
+
   virtual void OnUpdate() {};
   virtual void OnDestroy() {};
   virtual void OnFixedUpdate() {};
+
+  GameObj* GetGameObj() 
+  {
+    return parentObj;
+  }
+
+  Component(GameObj* _parent)
+  {
+    parentObj = _parent;
+  }
 
 };
 
---
+++ src/indigo/Transform.h
@@ -0,0 +1,11 @@
+#ifndef __IND_TRANSFORM__
+#define __IND_TRANSFORM__
+#include "Component.h"
+
+class Transform : public Component
+{
+
+
+};
+
+#endif
