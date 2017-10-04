6d8fb106536852190bddbf76604a3e47b5400f68
Grigler <graham.rigler@gmail.com>
2017-10-04 13:58:32 +0100

    Inital Application header

---
+++ src/indigo/Application.h
@@ -0,0 +1,20 @@
+#ifndef __IND_APPLICATION__
+#define __IND_APPLICATION__
+
+namespace Indigo
+{
+
+  class Application
+  {
+  public:
+
+    static void Init();
+    static void Kill();
+
+  private:
+
+  };
+
+}
+
+#endif
