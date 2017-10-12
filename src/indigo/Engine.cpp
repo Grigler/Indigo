e78af54610a8f65fa5f30f85d01047de2d7344df
Grigler <graham.rigler@gmail.com>
2017-10-12 09:26:23 +0100

    Removed unnecessary memory checks

--- src/indigo/Engine.cpp
+++ src/indigo/Engine.cpp
@@ -14,53 +14,24 @@ Engine::Engine()
 }
 Engine::~Engine()
 {
-  for (size_t i = 0; i < allMemObjs.size(); i++)
-  {
-    //allMemObjs.at(i).reset();
-  }
-  //allMemObjs.clear();
 
-  for (size_t i = 0; i < gameObjects.size(); i++)
-  {
-    //gameObjects.at(i).reset();
-  }
-  //gameObjects.clear();
 }
 
 void Engine::Update()
 {
   for (auto i = gameObjects.begin(); i != gameObjects.end(); i++)
   {
-    GameObject *obj = (*i).get();
-    if (obj == nullptr)
-    {
-      //stuff
-      Application::ErrPrint(std::exception("gameObjects list holds nullptr"));
-    }
-    else
-    {
-      obj->Update();
-    }
+    (*i)->Update();
   }
 
   for (auto i = gameObjects.begin(); i != gameObjects.end(); i++)
   {
-    GameObject *obj = (*i).get();
-    if (obj == nullptr)
-    {
-      //This shouldn't happen
-      Application::ErrPrint(std::exception("gameObjects list holds nullptr"));
-    }
-    else
-    {
-      obj->LateUpdate();
-    }
+    (*i)->LateUpdate();
   }
 
   for (auto i = messageQueue.begin(); i != messageQueue.end(); i++)
   {
-    Message m = (*i);
-    m.to.lock()->RecieveMessage(m.msg, m.from);
+    (*i).to.lock()->RecieveMessage((*i).msg, (*i).from);
   }
   messageQueue.clear();
 }
@@ -107,20 +78,6 @@ std::weak_ptr<MemObj> Engine::GetMemObjRef(MemObj *_obj)
   }
   return std::weak_ptr<MemObj>();
 }
-/*
-void Engine::RegisterGameObject(GameObject *_obj)
-{
-  for (auto i = gameObjects.begin(); i != gameObjects.end(); i++)
-  {
-    if ((*i).get() == _obj)
-    {
-      Application::ErrPrint("Repeated GameObject Registration");
-      return;
-    }
-  }
-  gameObjects.push_back(std::shared_ptr<GameObject>(_obj));
-}
-*/
 void Engine::RegisterGameObject(std::shared_ptr<GameObject> _obj)
 {
   gameObjects.push_back(_obj);
