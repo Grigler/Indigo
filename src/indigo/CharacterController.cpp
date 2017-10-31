f3327c69f33f0e7c0ef16b399cc80600ac6dad0b
Grigler <graham.rigler@gmail.com>
2017-10-31 15:27:17 +0000

    Split up character controller and reorder for rot first

--- src/indigo/CharacterController.cpp
+++ src/indigo/CharacterController.cpp
@@ -23,10 +23,33 @@ void CharacterController::onCreation()
 
 void CharacterController::onUpdate()
 {
-  glm::vec3 moveVec = glm::vec3(0);
+  UpdateRotFromMouse();
 
+  UpdatePosFromKeys();
+}
+
+void CharacterController::UpdateRotFromMouse()
+{
   std::weak_ptr<Transform> t = parent.lock()->transform;
 
+  glm::vec2 mouseDelta = Input::GetMouseDelta() * 10.0f;
+
+  float angleRotY = glm::sign(mouseDelta.x) * glm::degrees(glm::angle(glm::vec2(0, 1),
+    glm::normalize(glm::vec2(mouseDelta.x, 1)))) * Application::GetDT() * mouseSens.y;
+  float angleRotX = glm::sign(mouseDelta.y) * glm::degrees(glm::angle(glm::vec2(0, 1),
+    glm::normalize(glm::vec2(mouseDelta.y, 1)))) * Application::GetDT() * mouseSens.x;
+
+  angleRotX /= 10.0f;
+  angleRotY /= 10.0f;
+
+  t.lock()->Rotate(glm::vec3(angleRotX, angleRotY, 0.0f));
+
+  lastMouseDelta = mouseDelta;
+}
+void CharacterController::UpdatePosFromKeys()
+{
+  glm::vec3 moveVec = glm::vec3(0);
+  std::weak_ptr<Transform> t = parent.lock()->transform;
   if (Input::GetKey('w') || Input::GetKey('W'))
   {
     moveVec += t.lock()->GetForward() * moveSpeed;
@@ -46,33 +69,4 @@ void CharacterController::onUpdate()
   }
 
   t.lock()->SetPosition(t.lock()->GetPosition() + moveVec * Application::GetDT());
-
-  glm::vec2 mouseDelta = Input::GetMouseDelta() * 10.0f;
-
-  printf("R:%i\n", rand() % 255);
-
-  printf("D: %f %f\n", mouseDelta.x, mouseDelta.y);
-  
-  float angleRotY = glm::sign(mouseDelta.x) * glm::degrees(glm::angle(glm::vec2(0, 1),
-    glm::normalize(glm::vec2(mouseDelta.x, 1)))) * Application::GetDT() * mouseSens.y;
-  float angleRotX = glm::sign(mouseDelta.y) * glm::degrees(glm::angle(glm::vec2(0, 1),
-    glm::normalize(glm::vec2(mouseDelta.y, 1)))) * Application::GetDT() * mouseSens.x;
-
-  angleRotX /= 10.0f;
-  angleRotY /= 10.0f;
-
-  printf("Angles: %f %f\n", angleRotX, angleRotY);
-  /*
-  glm::mat4 rotationMat = glm::mat4(1);
-  rotationMat = glm::rotate(rotationMat, angleRotY, glm::vec3(0, 1, 0));
-  rotationMat = glm::rotate(rotationMat, angleRotX, glm::vec3(1, 0, 0));
-
-  t.lock()->SetRotation(glm::vec4(t.lock()->GetRotation(),1)*rotationMat);
-  */
-  t.lock()->Rotate(glm::vec3(angleRotX, angleRotY, 0.0f));
-
-  lastMouseDelta = mouseDelta;
-
-  //Manually calling cam component update event
-  //cam.onUpdate();
 }
--- src/indigo/CharacterController.h
+++ src/indigo/CharacterController.h
@@ -24,6 +24,9 @@ namespace Indigo
 
   private:
     glm::vec2 lastMouseDelta;
+
+    void UpdateRotFromMouse();
+    void UpdatePosFromKeys();
   };
 }
 
