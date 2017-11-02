a0adf8becfde3dbd067a11bec75a07360269c159
Grigler <graham.rigler@gmail.com>
2017-11-02 12:55:54 +0000

    Sound class starting

--- src/indigo/AudioManager.cpp
+++ src/indigo/AudioManager.cpp
@@ -22,7 +22,7 @@ bool AudioManager::Init()
   {
     return false;
   }
-
+  
   return true;
 }
 void AudioManager::Destroy()
@@ -36,4 +36,38 @@ void AudioManager::Destroy()
   alcMakeContextCurrent(NULL);
   alcDestroyContext(context);
   alcCloseDevice(device);
+}
+
+void AudioManager::Update()
+{
+  for (auto i = activeSources.begin(); i < activeSources.end(); i++)
+  {
+    ALint state = 0;
+    alGetSourcei((*i).lock()->id, AL_SOURCE_STATE, &state);
+    if (state == AL_STOPPED)
+    {
+      alDeleteSources(1, &(*i).lock()->id);
+      activeSources.erase(i);
+      i--;
+    }
+
+  }
+}
+
+std::weak_ptr<Sound> AudioManager::Load(std::string _path)
+{
+
+  return std::weak_ptr<Sound>();
+}
+
+
+std::vector<std::shared_ptr<Sound>> sounds;
+
+void Sound::Play()
+{
+
+}
+void Sound::PlayWithVariance(float _min, float _max)
+{
+
 }
--- src/indigo/AudioManager.h
+++ src/indigo/AudioManager.h
@@ -1,17 +1,25 @@
+#ifndef __IND_AUDIO_MANAGER__
+#define __IND_AUDIO_MANAGER__
+
+//Defined in config - used to switch between
+//dllimport (for engine) and dllexport (for dll build)
 #ifndef IND_AUDIO_OPENAL
 #define A_API __declspec(dllexport)
 #else
 #define A_API __declspec(dllimport)
 #endif
 
-#ifndef __IND_AUDIO_MANAGER__
-#define __IND_AUDIO_MANAGER__
-
 #include <AL/al.h>
 #include <AL/alc.h>
 
+#include <memory>
+#include <vector>
+#include <string>
+
 namespace Indigo
 {
+  class Sound;
+
   class AudioManager
   {
     friend class Engine;
@@ -19,9 +27,31 @@ namespace Indigo
     A_API bool Init();
     A_API void Destroy();
 
+    A_API void Update();
+
+    static A_API std::weak_ptr<Sound> Load(std::string _path);
+
   private:
     ALCdevice *device;
     ALCcontext *context;
+
+    static std::vector<std::shared_ptr<Sound>> loadedSounds;
+
+    std::vector<std::weak_ptr<Sound>> activeSources;
+  };
+
+  class Sound
+  {
+    friend class AudioManager;
+  public:
+    void Play();
+    //Varies pitch with a multiplyer between min and max
+    void PlayWithVariance(float _min, float _max);
+    
+  private:
+    std::string path;
+
+    ALuint id;
 
   };
 }
